780a6f18c9bd4f53d8c51b7151961ab4f531cdbe
KYs <116732010+Amitkys@users.noreply.github.com>
2023-12-30 20:02:21 +0530

    Create Winner of an election.cpp

---
+++ GeeksforGeeks/POTD/30dec 2023/Winner of an election.cpp
@@ -0,0 +1,23 @@
+class Solution{
+  public:
+  
+    //Function to return the name of candidate that received maximum votes.
+    vector<string> winner(string arr[],int n)
+    {
+      map<string,int> mp;
+        for(int i=0;i<n;i++) mp[arr[i]]++;
+        vector<string> ans;
+        string a="";
+        int v=0;
+        for(auto x:mp) v=max(v,x.second);
+        for(auto x:mp) {
+            if(x.second==v){
+               a=x.first;
+               break;
+            } 
+        }
+         ans.push_back(a);
+         ans.push_back(to_string(v));
+         return ans;  
+    }
+};
