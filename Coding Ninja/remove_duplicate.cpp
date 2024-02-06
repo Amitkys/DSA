158f5806fe672fe644a078918709e7d48dddea6a
Amitkys <amitkys59@gmail.com>
2024-02-06 17:49:25 +0530

    Remove duplicate element from unsorted array

---
+++ Coding Ninja/remove_duplicate.cpp
@@ -0,0 +1,10 @@
+int removeDuplicates(vector<int> &arr, int n) {
+	int j = 0;
+    for(int i = 1; i < n; i++){
+        if(arr[j] != arr[i]){
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+    return (j+1);
+}
