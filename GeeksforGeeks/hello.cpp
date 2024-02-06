6c1e3f4cb5fafa7fac77a1573964e5838101f430
Amitkys <amitkys59@gmail.com>
2024-02-06 12:46:12 +0530

    is array is sorted

--- GeeksforGeeks/hello.cpp
+++ GeeksforGeeks/hello.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    cout << "Github Checking";
+    cout << "Enter list of array: ";
+    int n;
+    cin >> n;
+    cout << " Entered number is : " << n;
+    int arr[n];
+    for(int i = 0;i < n; i++){
+        
+    }
 }
