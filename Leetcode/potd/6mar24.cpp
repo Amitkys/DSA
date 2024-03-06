d5c88e7d19fba77ba733d69954bd67d0dcab6a14
Amitkys <amitkys59@gmail.com>
2024-03-06 18:45:53 +0530

    Solved problem of the day on leetcode: 6mar, 24

---
+++ Leetcode/potd/6mar24.cpp
@@ -0,0 +1,14 @@
+// problem link : https://leetcode.com/problems/linked-list-cycle/submissions/1195719714/
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        
+        return false;
+    }
+};
